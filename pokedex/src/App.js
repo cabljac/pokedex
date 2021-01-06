56cd027f6e4510bd2f7f61ba8e44293d05610939
JacobCable94 <jacobcable94@gmail.com>
2021-01-06 12:32:04 +0000

    fixed responsiveness in easy way

--- pokedex/src/App.js
+++ pokedex/src/App.js
@@ -8,16 +8,31 @@ import Nav from './components/Nav'
 import PokeContainer from './components/PokeContainer'
 import Pokemons from './components/Pokemons'
 
+import useWindowSize from './hooks/useWindowSize'
+
+
 function App() {
+  const {width, height} = useWindowSize()
   const [number, setNumber] = useState(0)
-  return (
-    <div className="App">
-      <PokeContainer>
-        <Pokemons number={number} />        
-      </PokeContainer>
-      <Nav number={number} onClick={setNumber} />
-    </div>
-  );
+
+
+  if (width > 500 && height > 400) {
+    return (
+      <div className="App">
+        <PokeContainer>
+          <Pokemons number={number} />
+        </PokeContainer>
+        <Nav number={number} onClick={setNumber} />
+      </div>
+    );
+  } else {
+    return (
+      <div className="App">
+        <Pokemons number={number} />
+        <Nav number={number} onClick={setNumber} />
+      </div>
+    );
+  }
 }
 
 export default App;
