baa786c9cbcc43d05dcc9898e7416c04095e9bf7
JacobCable94 <jacobcable94@gmail.com>
2021-01-11 21:54:54 +0000

    finished typing

--- pokedex/src/lit/PokeName.ts
+++ pokedex/src/lit/PokeName.ts
@@ -3,20 +3,24 @@ import { LitElement, html } from 'lit-element';
 
 class PokeName extends LitElement {
 
+  vowels: string[] = ["A", "E", "I", "O", "U"];
+  name: string;
+
   static get properties() {
     return {
       name: { type: String }
     };
   }
 
+  constructor(nameInput: string) {
+    super()
+    this.name = "pokemon name"
+  }
 
   render() {
-    const vowels = ["A", "E", "I", "O", "U"];
-    // @ts-ignore
-    const name = this.name;
     return html`
       <h2>
-        It's ${vowels.includes(name.charAt(0)) ? "an" : "a"} ${name}!
+        It's ${this.vowels.includes(this.name.charAt(0)) ? "an" : "a"} ${this.name}!
       </h2>
     `;
   }
